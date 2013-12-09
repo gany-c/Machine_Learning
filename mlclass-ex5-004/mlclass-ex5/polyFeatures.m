6700f5c53c43517f3d436ee9a1d06c155a0c7eb8
Gany Chidambaram <ganapathy.c@gmail.com>
2013-12-08 16:34:08 -0800

    convert a vector of features to a matrix of features

--- mlclass-ex5-004/mlclass-ex5/polyFeatures.m
+++ mlclass-ex5-004/mlclass-ex5/polyFeatures.m
@@ -4,18 +4,31 @@ function [X_poly] = polyFeatures(X, p)
 %   maps each example into its polynomial features where
 %   X_poly(i, :) = [X(i) X(i).^2 X(i).^3 ...  X(i).^p];
 %
+disp("X");
+disp(size(X));
 
-
+disp("p");
+disp(p);
 % You need to return the following variables correctly.
-X_poly = zeros(numel(X), p);
 
+X_poly = zeros(numel(X), p);
+disp("X_poly");
+disp(size(X_poly));
+
+m = size(X_poly,1);
+disp("m");
+disp(m);
 % ====================== YOUR CODE HERE ======================
 % Instructions: Given a vector X, return a matrix X_poly where the p-th 
 %               column of X contains the values of X to the p-th power.
 %
 % 
 
-
+for i = 1:m,
+	for j = 1:p,
+		X_poly(i,j) = X(i) ^ j;
+	end
+end 
 
 
 
