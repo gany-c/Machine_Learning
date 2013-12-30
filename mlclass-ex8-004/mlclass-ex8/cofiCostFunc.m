aeedfc1fd20e0385e47d4d642b8b85bd182f276e
Gany Chidambaram <ganapathy.c@gmail.com>
2013-12-29 20:48:01 -0800

    sum of squares of differences between predictions and actual user ratings

--- mlclass-ex8-004/mlclass-ex8/cofiCostFunc.m
+++ mlclass-ex8-004/mlclass-ex8/cofiCostFunc.m
@@ -11,7 +11,6 @@ X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
-            
 % You need to return the following values correctly
 J = 0;
 X_grad = zeros(size(X));
@@ -41,6 +40,12 @@ Theta_grad = zeros(size(Theta));
 %
 
 
+predictions = X * Theta';
+differences = predictions .- Y;
+validDiff = differences .* R;
+validDiff = validDiff .^ 2;
+doubCost = sum(sum(validDiff));
+J = doubCost/2; 
 
 
 
